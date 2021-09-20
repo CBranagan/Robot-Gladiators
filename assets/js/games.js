d4d977c6ec9ca99854a48f9c9b91e59a8721523a
Chad Branagan <chad.branagan@gmail.com>
2021-09-20 10:38:35 -0700

    learned functions and how to store user data

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! Javascript is running!");
+var playerName = window.prompt("What is your Robot's Name?");
+
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a message");
+
+console.log(10+10);
+
+console.log("Our robot's name is " + playerName);
+
+function fight() {
+    window.alert("The Fight has begun!");
+}
+
+
+// fight();
